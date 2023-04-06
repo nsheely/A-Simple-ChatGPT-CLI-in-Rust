99389b238e5cf714e33ba1839435327a26bad85c
Sheely <nathansheely@gmail.com>
2023-04-06 18:30:45 -0500

    git actions fix 3

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use clap::{App, Arg};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::io::{self, BufRead};
 use std::fmt;
+use std::io::{self, BufRead};
 
-/* 
+/*
    This ChatGPT Rust CLI program allows users to interact with OpenAI's ChatGPT using
    the command-line interface.
 
-   The main function initializes the necessary configuration, such as the API key, 
-   API URL, and the chat model to use. It then checks for command-line arguments 
+   The main function initializes the necessary configuration, such as the API key,
+   API URL, and the chat model to use. It then checks for command-line arguments
    and decides the appropriate mode to run:
 
-   1. Non-Interactive Single Message Mode: If a message is provided as a command-line 
+   1. Non-Interactive Single Message Mode: If a message is provided as a command-line
       argument, the program sends this single message to ChatGPT and displays the response.
-   2. Non-Interactive Stdin Message Mode: If no arguments are provided, the program 
+   2. Non-Interactive Stdin Message Mode: If no arguments are provided, the program
       reads a single message from stdin, sends it to ChatGPT, and displays the response.
-   3. Interactive Mode: If started with the --interactive flag, the program enters a chat loop 
-      that continuously takes user input, sends it to ChatGPT, and displays the response. The loop 
+   3. Interactive Mode: If started with the --interactive flag, the program enters a chat loop
+      that continuously takes user input, sends it to ChatGPT, and displays the response. The loop
       continues until the user enters "exit."
 
    To communicate with ChatGPT, the program uses the `chat` function, which sends an HTTP request
@@ -36,7 +36,7 @@ impl fmt::Display for CustomError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             CustomError::ReqwestError(e) => write!(f, "Reqwest error: {}", e), // Format the ReqwestError variant.
-            CustomError::ParseError(s) => write!(f, "Parse error: {}", s),    // Format the ParseError variant.
+            CustomError::ParseError(s) => write!(f, "Parse error: {}", s), // Format the ParseError variant.
         }
     }
 }
@@ -51,15 +51,15 @@ impl From<reqwest::Error> for CustomError {
 // Structure for serializing the chat request to the OpenAI API.
 #[derive(Serialize)]
 struct ChatRequest<'a> {
-    model: &'a str,            // The model to use for the API call.
-    messages: &'a [Message],   // The slice containing the conversation messages.
+    model: &'a str,          // The model to use for the API call.
+    messages: &'a [Message], // The slice containing the conversation messages.
 }
 
 // Structure for storing a message within the conversation.
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Message {
-    role: String,     // The role of the message's author ("user", "assistant", or "system").
-    content: String,  // The content of the message.
+    role: String,    // The role of the message's author ("user", "assistant", or "system").
+    content: String, // The content of the message.
 }
 
 // Structure for deserializing the chat response from the OpenAI API.
@@ -76,11 +76,11 @@ struct Choice {
 
 // This function sends a chat request to the OpenAI API and receives the response.
 async fn chat(
-    client: &Client,          // The reqwest client for making API requests.
-    api_key: &str,            // The OpenAI API key.
-    api_url: &str,            // The OpenAI API URL.
-    model: &str,              // The model to use for the API call.
-    messages: &[Message],     // The slice containing the conversation messages.
+    client: &Client,      // The reqwest client for making API requests.
+    api_key: &str,        // The OpenAI API key.
+    api_url: &str,        // The OpenAI API URL.
+    model: &str,          // The model to use for the API call.
+    messages: &[Message], // The slice containing the conversation messages.
 ) -> Result<Message, CustomError> {
     // Create a ChatRequest object using the provided model and messages.
     let request = ChatRequest { model, messages };
@@ -113,10 +113,11 @@ async fn chat(
     } else {
         // If deserialization fails, print the raw response and return a custom error.
         println!("Raw response: {}", response_text);
-        Err(CustomError::ParseError("Error parsing the API response".to_string()))
+        Err(CustomError::ParseError(
+            "Error parsing the API response".to_string(),
+        ))
     }
 }
-
 
 // This function implements the single message mode for the chat application.
 async fn single_message_mode(
@@ -220,13 +221,30 @@ async fn main() {
 
     // If there is a command-line argument, use single-message mode.
     if let Some(input) = matches.value_of("input") {
-        single_message_mode(&client, &api_key, api_url, model, input.trim(), &mut messages).await;
-    } else if matches.is_present("interactive") { // If the interactive flag is present, use interactive mode.
+        single_message_mode(
+            &client,
+            &api_key,
+            api_url,
+            model,
+            input.trim(),
+            &mut messages,
+        )
+        .await;
+    } else if matches.is_present("interactive") {
+        // If the interactive flag is present, use interactive mode.
         interactive_mode(&client, &api_key, api_url, model, &stdin, &mut messages).await;
     } else {
         // If no argument or flag, use non-interactive mode: single message from stdin.
         let mut input = String::new();
         stdin.lock().read_line(&mut input).unwrap();
-        single_message_mode(&client, &api_key, api_url, model, input.trim(), &mut messages).await;
+        single_message_mode(
+            &client,
+            &api_key,
+            api_url,
+            model,
+            input.trim(),
+            &mut messages,
+        )
+        .await;
     }
 }
